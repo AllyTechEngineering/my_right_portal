a5d1785ba99e925dc352ed6a971f12e6cc5e5fdb
Bob Taylor <btaylor@allytechllc.com>
2025-04-09 15:49:25 -0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_right_portal/firebase_options.dart';
+import 'package:my_right_portal/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Right Portal',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0441B2)),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
